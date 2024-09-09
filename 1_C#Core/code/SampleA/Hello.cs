cc013955f244dbf62e219d807b7ef97b9ee7cc24
“vkrishnanb” <vkrishnanb@yahoo.com>
2024-09-09 13:15:50 +0530

    moved basic lessons

--- 1_C#Core/code/SampleA/Hello.cs
+++
@@ -1,8 +0,0 @@
-using System;
-public class Hello
-{
-    public static  void SayHello()
-    {
-        Console.WriteLine("Hello");
-    }
-}
--- 1_C#Core/code/dotnetcore/SampleB/Calculator.cs
+++
@@ -1,4 +0,0 @@
-public class Calculator
-{
-   public void Add(int x,int y){System.Console.WriteLine(x+y);}
-}
